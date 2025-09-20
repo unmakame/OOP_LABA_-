2aa3beb02745712a79df8490eb38505036ef90a7
unmakame <bayanovlad@gmail.com>
2025-09-20 14:36:00 +0300

    create gitignor + fix copy

--- src/change_letter.cpp
+++ src/change_letter.cpp
@@ -2,10 +2,7 @@
 #include <string>
 
 
-std::string changeLetter(std::string str_input){
-    // std::cout << "Input your string with letters a/b/c" << std::endl;
-    // std::cin >> str;
-    std::string str = str_input;
+std::string changeLetter(std::string str){
     for(int i = 0; i < str.length(); i++){
         if (str[i] == 'a'){
             str[i] = 'b';
